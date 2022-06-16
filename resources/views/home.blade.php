279bea50ca558573ff94be965adbcdcb9f6d590e
Manhisme09 <manht9090@gmail.com>
2022-06-16 17:19:53 +0700

    header banner course

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,49 @@
 
 @section('content')
 
+<section>
+    <div class="banner" style="background-image: url('{{ asset('images/banner.png') }}');">
+        <div class="banner_title">
+            <p class="banner_title_first">Learn Anytime, Anywhere <br> <span>at HapoLearn!</span>
+            </p>
+            <p class="banner_title_second">Interactive lessons, "on-the-go" <br> practice, peer support.</p>
+            <div class="btn-start">
+                <p class="btn-start-title">Start Learning Now!</p>
+            </div>
+        </div>
+    </div>
+    <div class="banner_bonus"></div>
+</section>
+
 <section class="example">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                        Hello World!!!
-                    </div>
+    <div class="container_course">
+        <div class="course one">
+            <div class="course_image"><div class="logo"></div></div>
+            <div class="course_title">
+                <p class="name">HTML/CSS/js Tutorial</p>
+                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <div class="btn_take">
+                    <p class="btn_take_title">Take This Course</p>
+                </div>
+            </div>
+        </div>
+        <div class="course two">
+            <div class="course_image"><div class="logo"></div></div>
+            <div class="course_title">
+                <p class="name">LARAVEL Tutorial</p>
+                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <div class="btn_take">
+                    <p class="btn_take_title">Take This Course</p>
+                </div>
+            </div>
+        </div>
+        <div class="course three">
+            <div class="course_image"><div class="logo"></div></div>
+            <div class="course_title">
+                <p class="name disable">PHP Tutorial</p>
+                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had coded quite a bit, but never touched anything in regards to web development.</p>
+                <div class="btn_take">
+                    <p class="btn_take_title">Take This Course</p>
                 </div>
             </div>
         </div>
