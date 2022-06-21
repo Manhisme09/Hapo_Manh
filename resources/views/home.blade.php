138c7b7f4df03f64a3ff58088d9dbbe2287d3f18
Manhisme09 <manht9090@gmail.com>
2022-06-21 16:53:55 +0700

    #3 fix next

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,50 +14,5 @@
             </div>
         </div>
     </div>
-    {{-- <div class="banner_bonus"></div> --}}
 </section>
-{{-- <section class="example">
-    <div class="container_course">
-        <div class="course one">
-            <div class="course_image">
-                <div class="logo"></div>
-            </div>
-            <div class="course_title">
-                <p class="name">HTML/CSS/js Tutorial</p>
-                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had
-                    coded quite a bit, but never touched anything in regards to web development.</p>
-                <div class="btn_take">
-                    <p class="btn_take_title">Take This Course</p>
-                </div>
-            </div>
-        </div>
-        <div class="course two">
-            <div class="course_image">
-                <div class="logo"></div>
-            </div>
-            <div class="course_title">
-                <p class="name">LARAVEL Tutorial</p>
-                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had
-                    coded quite a bit, but never touched anything in regards to web development.</p>
-                <div class="btn_take">
-                    <p class="btn_take_title">Take This Course</p>
-                </div>
-            </div>
-        </div>
-        <div class="course three">
-            <div class="course_image">
-                <div class="logo"></div>
-            </div>
-            <div class="course_title">
-                <p class="name disable">PHP Tutorial</p>
-                <p class="description">I knew hardly anything about HTML, JS, and CSS before entering New Media. I had
-                    coded quite a bit, but never touched anything in regards to web development.</p>
-                <div class="btn_take">
-                    <p class="btn_take_title">Take This Course</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section> --}}
 @endsection
