7b80167dd964fc3cd6c95102746e4b5d86508f17
Manhisme09 <manht9090@gmail.com>
2022-06-23 08:44:56 +0700

    #3 fix convention nexttt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
 
 <section>
     <div class="banner" style="background-image: url('{{ asset('images/banner.png') }}');">
-        <div class="banner_title">
-            <p class="banner_title_first">Learn Anytime, Anywhere <br> <span>at HapoLearn<span><img
+        <div class="banner-title">
+            <p class="banner-title-first">Learn Anytime, Anywhere <br> <span>at HapoLearn<span><img
                             src="{{ asset('images/concu.png') }}" alt=""></span>!</span>
             </p>
-            <p class="banner_title_second">Interactive lessons, "on-the-go" <br> practice, peer support.</p>
+            <p class="banner-title-second">Interactive lessons, "on-the-go" <br> practice, peer support.</p>
             <div class="btn-start">
                 <p class="btn-start-title">Start Learning Now!</p>
             </div>
